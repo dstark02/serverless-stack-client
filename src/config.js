8ecc54bb48c99ea8fd943be0cbd784c9b270cd20
David George Stark <dstark02@qub.ac.uk>
2021-02-20 12:41:42 +0000

    Adding login, signup and authentication to app

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
 const config = {
     s3: {
-      REGION: "eu-west-1",
-      BUCKET: "ds-notes-app-upload",
+        REGION: "eu-west-1",
+        BUCKET: "ds-notes-app-upload",
     },
     apiGateway: {
-      REGION: "eu-west-1",
-      URL: "https://x9h3jo4725.execute-api.eu-west-1.amazonaws.com/prod",
+        REGION: "eu-west-1",
+        URL: "https://x9h3jo4725.execute-api.eu-west-1.amazonaws.com/prod",
     },
     cognito: {
-      REGION: "eu-west-1",
-      USER_POOL_ID: "eu-west-1_RFnzZ8T6x",
-      APP_CLIENT_ID: "4k5t3n1000sjtiakj4bocdrkqr",
-      IDENTITY_POOL_ID: "eu-west-1:010fc61a-8704-4809-9a39-e7088deb6fe3",
+        REGION: "eu-west-1",
+        USER_POOL_ID: "eu-west-1_RFnzZ8T6x",
+        APP_CLIENT_ID: "4k5t3n1000sjtiakj4bocdrkqr",
+        IDENTITY_POOL_ID: "eu-west-1:010fc61a-8704-4809-9a39-e7088deb6fe3",
     },
-  };
-  
-  export default config;
+};
+
+export default config;
